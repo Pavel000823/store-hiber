ad54e2f7050f4719322051af0295963c06eae565
Павел <pavel.ulkin@gmail.com>
2022-06-05 13:09:17 +0300

    Initial commit

---
+++ src/main/java/com/geek/app/MainApp.java
@@ -0,0 +1,13 @@
+package com.geek.app;
+
+import com.geek.app.config.ConfigApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigApplication.class);
+
+    }
+}
---
+++ src/main/java/com/geek/app/config/ConfigApplication.java
@@ -0,0 +1,11 @@
+package com.geek.app.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.geek.app")
+public class ConfigApplication {
+
+}
